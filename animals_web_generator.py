dec16e9386133ca786718f1ad7410a1ce706f3aa
JQSwain <j.q.swain@hotmail.com>
2025-06-14 16:10:53 +0200

    Sixth Commit, improved card item html, changed problem with json file and one character in Darwin's Fox

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -15,15 +15,16 @@ for animal in animals_data:
         type = animal["characteristics"]["type"]
     except KeyError:
         type = None
-    output += '<li class="cards_item">'
-    output += f"Name: {name}<br/>\n"
-    output += f"Diet: {diet}<br/>\n"
-    output += f"Location: {location}<br/>\n"
+    output += '<li class="cards__item">'
+    output += f'<div class="card__title">{name}</div>\n'
+    output += f'<p class="card__text">\n'
+    output += f'<strong>Diet:</strong> {diet}<br/>\n'
+    output += f'<strong>Location:</strong> {location}<br/>\n'
     if type is not None:
-        output += f"Type: {type}<br/>\n"
+        output += f'<strong>Type:</strong> {type}<br/>\n'
     else:
         output += f""
-    output += '<br/></li>\n'
+    output += '</p></li>\n'
 print(output)
 
 with open("animals_template.html", "r") as template:
